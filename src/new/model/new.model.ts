b131110c8e630e7709f43da77b944774f849102a
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-11-08 11:57:32 -0300

    feat(auth): implements auth

--- src/new/model/new.model.ts
+++ src/new/model/new.model.ts
@@ -3,7 +3,6 @@ import { Document } from 'mongoose';
 export interface New extends Document {
   readonly title: string;
   readonly lead: string;
-  readonly date: string;
   readonly content: string;
   readonly image: string;
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDTO, UpdateUserDTO } from './dto/user.dto';
+import { User } from './model/user.model';
+import * as bcrypt from 'bcrypt';
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel('user')
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async createUser(createUserDTO: CreateUserDTO) {
+    const newUser = new this.userModel(createUserDTO);
+    const hashPassword = await bcrypt.hash(newUser.password, 10);
+    newUser.password = hashPassword;
+    const model = await newUser.save();
+    const { password, ...rta } = model.toJSON();
+    return rta;
+  }
+
+  async getUsers(): Promise<User[]> {
+    const users = await this.userModel.find();
+    return users;
+  }
+
+  async getUser(id: string): Promise<User> {
+    const user = await this.userModel.findById(id);
+    return user;
+  }
+
+  async deleteUser(id: string): Promise<any> {
+    const user = await this.userModel.findByIdAndDelete(id);
+    return user;
+  }
+
+  async updateUser(id: string, updateUserDTO: UpdateUserDTO): Promise<User> {
+    const updatedUser = await this.userModel
+      .findByIdAndUpdate(id, { $set: updateUserDTO }, { new: true })
+      .exec();
+    return updatedUser;
+  }
+
+  findByEmail(email: string) {
+    return this.userModel.findOne({ email }).exec();
+  }
+}
